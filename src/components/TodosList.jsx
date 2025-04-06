80f6e26dd937270aca0d64bfedeea3bf2ec9624c
Matthew D Wilson <mattrick2214@gmail.com>
2025-04-06 03:02:30 -0500

    const initialTodos

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,5 +1,11 @@
 import Todo from './Todo.jsx';
 
+const initialTodos = [
+    { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
+    { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false},
+    { id: 2, title: 'Learn Redux', description: 'Learn state management with Redux', isDone: false }
+];
+
 function TodosList() {
 
   return (
