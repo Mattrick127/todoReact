09e31d7272f2fc681097c7b80ef885e0c5ee2936
Matthew D Wilson <mattrick2214@gmail.com>
2025-04-06 19:11:37 -0500

    code cleanup

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,37 +1,47 @@
+import { TodosContext } from '../TodosContext';
+import { useContext } from 'react';
 import './Todo.scss';
 
-function Todo({ todo, deleteTodo, toggleIsDone }) {
-
-  return (
-    <>
-        <div className={`todo ${todo.isDone ? 'done' : ''}`}>
-                <button 
-                onClick={()=> deleteTodo(todo.id)}
-                className="erase"
-            >
-                    x erase
-                </button>
-            <h3>
-                {todo.title}
-            </h3>
-            <p>
-                {todo.description}
-            </p>
-            <div className="task-check">
-                <input
-                        
-                    onClick={()=> toggleIsDone(todo.id)}
-                    type="checkbox"
-                    defaultChecked={todo.isDone}
-                    />
-                <label>
-                    {!todo.isDone ? 'To-Do' : 'Done'}
-                </label>
+function Todo({ todo }) {
+    const store = useContext(TodosContext)
+    return (
+        <>
+            <div className={`todo ${todo.isDone ? 'done' : ''}`}>
+                    <button 
+                    onClick={()=> 
+                        store.dispatch({
+                          type: 'deleted',
+                          id: todo.id
+                        })}
+                    className="erase"
+                >
+                        x erase
+                    </button>
+                <h3>
+                    {todo.title}
+                </h3>
+                <p>
+                    {todo.description}
+                </p>
+                <div className="task-check">
+                    <input
+                            
+                        onClick={()=> 
+                            store.dispatch({
+                              type: 'toggledIsDone',
+                              id: todo.id
+                            })}
+                        type="checkbox"
+                        defaultChecked={todo.isDone}
+                        />
+                    <label>
+                        {!todo.isDone ? 'To-Do' : 'Done'}
+                    </label>
+                </div>
             </div>
-        </div>
 
-    </>
-  )
+        </>
+    )
 }
 
 export default Todo
