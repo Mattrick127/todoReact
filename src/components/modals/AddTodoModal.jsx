b30015819b82f4e35bff0212ff8f538c5a24b762
Matthew D Wilson <mattrick2214@gmail.com>
2025-04-08 13:29:21 -0500

    added message for no entry

--- src/components/modals/AddTodoModal.jsx
+++ src/components/modals/AddTodoModal.jsx
@@ -1,4 +1,20 @@
+import {useTodos} from '../../TodosContext.jsx';
+
+
 function AddTodoModal() {
+
+    const store = useTodos();
+
+    function addTaskHandler() {
+        let newTodo = {isDone: false};
+        newTodo.title = document.querySelector('input[name=title]').value;
+        newTodo.description = document.querySelector('textarea[name=description]').value
+        if (newTodo.title && newTodo.description) {
+            console.log('go ahead')
+        } else {
+            alert('Please enter the title and description for the todo.');
+        }
+    }
  
     return (
     <>
@@ -8,7 +24,7 @@ function AddTodoModal() {
             <input type="text" name="title" placeholder="Enter a title..." /><br />
             <label htmlFor="description">Description *</label>
             <textarea name="description" rows="4" placeholder="Enter a description..." /><br />
-            <button className="btn gray">Add Task</button>
+            <button onClick={addTaskHandler} className="btn gray">Add Task</button>
         </div>
     </>
     )
